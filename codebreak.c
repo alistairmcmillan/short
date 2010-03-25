72c755af942b37c7bf1a08aae35631e0bb4bb9f1
Jan Dlabal <admin@houbysoft.com>
2010-03-24 22:51:20 -0400

    Added quick help and possibility to play again without having to start the program again

--- codebreak.c
+++ codebreak.c
@@ -23,104 +23,125 @@ int need_space_int(int i)
 int main(int argc, char *argv[])
 {
   srand(time(NULL));
-  printf("Welcome to CodeBreak (http://houbysoft.com/en/downprog.php#codebreak).\n");
+  printf("Welcome to CodeBreak (http://houbysoft.com/en/downprog.php#codebreak).\nStart guessing, or type '?' or 'h' for quick help.\n");
   int min = 1000;
   int max = 10000;
   int deduceme;
   char *deducestr;
   char *statusstr;
-  while (1)
+  char again = 1;
+  while (again)
   {
-    deduceme = rand() % (max - min) + min;
-    deducestr = malloc(need_space_int(deduceme)+1);
-    statusstr = malloc(need_space_int(deduceme)+1);
-    if (!deducestr || !statusstr)
+    while (1)
     {
-      printf("Error : cannot allocate enough memory. Something is probably very wrong; closing.\n");
-      exit(1);
-    }
-    sprintf(deducestr,"%i",deduceme);
-    int i=0;
-    char digits[10];
-    memset(&digits, 0, 10);
-    for (; i<strlen(deducestr); i++)
-    {
-      if (digits[deducestr[i]-48]==1)
+      deduceme = rand() % (max - min) + min;
+      deducestr = malloc(need_space_int(deduceme)+1);
+      statusstr = malloc(need_space_int(deduceme)+1);
+      if (!deducestr || !statusstr)
       {
-	free(deducestr);
-	free(statusstr);
-	deduceme = 0;
-	break;
+	printf("Error : cannot allocate enough memory. Something is probably very wrong; closing.\n");
+	exit(1);
       }
-      else
-	digits[deducestr[i]-48]=1;
-    }
-    if (deduceme == 0)
-      continue;
-    else
-      break;
-  }
-  sprintf(statusstr,"");
-  int tries_remain = 12;
-  int guess = -1;
-  while (tries_remain)
-  {
-    printf("[%s] (%i %s)\n\n",statusstr,tries_remain,tries_remain == 1 ? "try remains" : "tries remain");
-    char *guessstr = malloc(need_space_int(max)+1);
-    if (!guessstr)
-    {
-      printf("Error : cannot allocate enough memory. Something is probably very wrong; closing.\n");
-      exit(1);
-    }
-    char first = 1;
-    guess = -1;
-    while (!(guess >= min && guess < max))
-    {
-      if (!first)
-	printf("Wrong format. Your guess needs to be an integer >= %i and < %i.\n",min,max);
-      first = 0;
-      fgets(guessstr,need_space_int(max)+1,stdin);
-      guessstr[need_space_int(max)] = 0;
-      if (guessstr[strlen(guessstr)-1]=='\n')
-	guessstr[strlen(guessstr)-1]=0;
-      guess = atoi(guessstr);
-    }
-    if (guess == deduceme)
-      break;
-    else
-    {
+      sprintf(deducestr,"%i",deduceme);
       int i=0;
-      int correct_pos = 0;
-      int correct_num = 0;
-      for (; i<strlen(guessstr); i++)
+      char digits[10];
+      memset(&digits, 0, 10);
+      for (; i<strlen(deducestr); i++)
       {
-	if (strchr(deducestr,guessstr[i]))
+	if (digits[deducestr[i]-48]==1)
 	{
-	  if ((strchr(deducestr, guessstr[i]) - deducestr) == i)
+	  free(deducestr);
+	  free(statusstr);
+	  deduceme = 0;
+	  break;
+	}
+	else
+	  digits[deducestr[i]-48]=1;
+      }
+      if (deduceme == 0)
+	continue;
+      else
+	break;
+    }
+    sprintf(statusstr,"");
+    int tries_remain = 12;
+    int guess = -1;
+    while (tries_remain)
+    {
+      printf("[%s] (%i %s)\n\n",statusstr,tries_remain,tries_remain == 1 ? "try remains" : "tries remain");
+      char *guessstr = malloc(need_space_int(max)+1);
+      if (!guessstr)
+      {
+	printf("Error : cannot allocate enough memory. Something is probably very wrong; closing.\n");
+	exit(1);
+      }
+      char first = 1;
+      guess = -1;
+      while (!(guess >= min && guess < max))
+      {
+	if (!first)
+	  printf("Wrong format. Your guess needs to be an integer >= %i and < %i.\n",min,max);
+	first = 0;
+	fgets(guessstr,need_space_int(max)+1,stdin);
+	guessstr[need_space_int(max)] = 0;
+	if (guessstr[strlen(guessstr)-1]=='\n')
+	  guessstr[strlen(guessstr)-1]=0;
+	else
+	  first = 1;
+	if (strcmp(guessstr,"h")==0 || strcmp(guessstr,"?")==0)
+	{
+	  printf("This game is really simple.\nYour goal is to deduce, by logic, the number the computer is 'thinking'. To do that, you can enter a 4-digit number 12 times.\nEvery entry will get evaluated by the computer and you will get a response in the form of circles, stars, and minuses.\nA star means that in your response, a number is in its correct position.\nA circle means that you included a correct number in your try, but it is not in the correct position.\nMinuses represent the number of numbers that you entered but are not in the answer.\n\nNote that these 'ratings' are not necessarily in the correct order. For example, if the answer is 1234, and you enter '5612', you will get a rating of [oo--], instead of [--oo], as you might have thought.\n\nGood luck, enter your first try!\n");
+	  first = 1;
+	}
+	guess = atoi(guessstr);
+      }
+      if (guess == deduceme)
+	break;
+      else
+      {
+	int i=0;
+	int correct_pos = 0;
+	int correct_num = 0;
+	for (; i<strlen(guessstr); i++)
+	{
+	  if (strchr(deducestr,guessstr[i]))
 	  {
-	    correct_pos += 1;
-	  } else {
-	    correct_num += 1;
+	    if ((strchr(deducestr, guessstr[i]) - deducestr) == i)
+	    {
+	      correct_pos += 1;
+	    } else {
+	      correct_num += 1;
+	    }
 	  }
 	}
+	int incorrect = strlen(deducestr) - correct_pos - correct_num;
+	strcpy(statusstr,"");
+	while (correct_pos--)
+	  strcat(statusstr,"*");
+	while (correct_num--)
+	  strcat(statusstr,"o");
+	while (incorrect--)
+	  strcat(statusstr,"-");
+	free(guessstr);
       }
-      int incorrect = strlen(deducestr) - correct_pos - correct_num;
-      strcpy(statusstr,"");
-      while (correct_pos--)
-	strcat(statusstr,"*");
-      while (correct_num--)
-	strcat(statusstr,"o");
-      while (incorrect--)
-	strcat(statusstr,"-");
-      free(guessstr);
+      tries_remain--;
     }
-    tries_remain--;
-  }
-  if (tries_remain)
-  {
-    printf("Congratulations; you won. You had %i more %s.\n",tries_remain-1,tries_remain-1 == 1 ? "try" : "tries");
-  } else {
-    printf("You lost; the answer was %i.\n",deduceme);
+    if (tries_remain)
+    {
+      printf("Congratulations; you won. You had %i more %s.\n",tries_remain-1,tries_remain-1 == 1 ? "try" : "tries");
+    } else {
+      printf("You lost; the answer was %i.\n",deduceme);
+    }
+    again = 0;
+    printf("Do you want to play again? (yes/no) ");
+    char ans[5];
+    fgets(ans,5,stdin);
+    if (strcmp(ans,"yes\n")==0)
+      again = 1;
+    else
+      again = 0;
+    free(deducestr);
+    free(statusstr);
   }
   return 0;
 }
