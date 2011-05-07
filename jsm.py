6ebf30e0fd23b78acf3921301fe7929125ed5b85
Jan Dlabal <admin@houbysoft.com>
2011-05-07 15:25:13 -0400

    Added jsm -- Just Send Mail, a simple python script to send small notification memails

---
+++ jsm.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+# JSM -- just send mail
+# simple script to Just Send Mail (jsm)
+# (c) Jan Dlabal, 2011.
+# http://houbysoft.com/
+# Provided under the GNU GPL v3 License.
+
+from sys import argv
+from email.mime.text import MIMEText
+import smtplib
+from DNS.lazy import mxlookup
+
+if len(argv) != 5:
+  print("Usage : " + argv[0] +" FROM TO SUBJECT MESSAGE")
+  print(" FROM is the email address to send from")
+  print(" TO is the email address to send to")
+  print(" SUBJECT is the email's subject")
+  print(" MESSAGE is the message to send")
+  print("\nThis program is (c) Jan Dlabal, 2011, and is available under the terms of the GNU GPL v3 License.")
+else:
+  msg = MIMEText(argv[4])
+  msg['Subject'] = argv[3]
+  msg['To'] = argv[2]
+  msg['From'] = argv[1]
+  try:
+    server = mxlookup(argv[2].split('@')[1])[0][1]
+  except:
+    print("Error : Could not get an MX record.")
+    exit(1)
+  print("Connecting to " + server)
+  s = smtplib.SMTP(server)
+  d = s.sendmail(argv[1], [argv[2]], msg.as_string())
+  if len(d) == 0:
+    print("Email sent successfully")
+  else:
+    print("Error occured while sending : " + repr(d))
+  s.quit()
+  exit(0)
+
