bb7dcbd05a3cd315d413a790a2e7455466077df4
Jan Dlabal <admin@houbysoft.com>
2012-06-26 15:35:10 -0400

    Added geometry utils

---
+++ jd-geometry.cpp
@@ -0,0 +1,87 @@
+#include <math.h>
+#include "jd-geometry.h"
+
+
+Vector::Vector(double x, double y, double z):Point(x,y,z) {
+}
+
+
+Vector::Vector():Point() {
+}
+
+
+double Vector::norm() {
+    return sqrt(pow(x(),2) + pow(y(),2) + pow(z(),2));
+}
+
+
+void Vector::normalize() {
+    double n = norm();
+    coords[0] /= n;
+    coords[1] /= n;
+    coords[2] /= n;
+}
+
+
+Vector Vector::crossProduct(Vector u, Vector v) {
+    Vector r;
+    r.setX(u[1]*v[2] - u[2]*v[1]);
+    r.setY(u[2]*v[0] - u[0]*v[2]);
+    r.setZ(u[0]*v[1] - u[1]*v[0]);
+    return r;
+}
+
+
+Vector Vector::crossProduct(Point a, Point b, Point c) {
+    Vector ab, ac;
+	ab[0] = b[0] - a[0];
+	ab[1] = b[1] - a[1];
+	ab[2] = b[2] - a[2];
+	ac[0] = c[0] - a[0];
+	ac[1] = c[1] - a[1];
+	ac[2] = c[2] - a[2];
+
+    Vector r(ab[1]*ac[2] - ab[2]*ac[1],
+             ab[2]*ac[0] - ab[0]*ac[2],
+             ab[0]*ac[1] - ab[1]*ac[0]);
+    return r;
+}
+
+
+double Vector::dotProduct(Vector u, Vector v) {
+    return u[0] * v[0] + u[1] * v[1] * u[2] * v[2];
+}
+
+
+double Quaternion::norm() {
+    return sqrt(pow(x,2) + pow(y,2) + pow(z,2) + pow(w,2));
+}
+
+
+void Quaternion::normalize() {
+    double n = norm();
+    x /= n;
+    y /= n;
+    z /= n;
+    w /= n;
+}
+
+
+Quaternion Quaternion::conjugate() {
+    Quaternion r;
+    r.w = w;
+    r.x = x*-1;
+    r.y = y*-1;
+    r.z = z*-1;
+    return r;
+}
+
+
+Quaternion operator*(Quaternion q, Quaternion r) {
+    Quaternion t;
+    t.w = r.w * q.w - r.x * q.x - r.y * q.y - r.z * q.z;
+    t.x = r.w * q.x + r.x * q.w - r.y * q.z + r.z * q.y;
+    t.y = r.w * q.y + r.x * q.z + r.y * q.w - r.z * q.x;
+    t.z = r.w * q.z - r.x * q.y + r.y * q.x + r.z * q.w;
+    return t;
+}
---
+++ jd-geometry.h
@@ -0,0 +1,73 @@
+#ifndef JD_GEOMETRY_H
+#define JD_GEOMETRY_H
+
+class Point {
+protected:
+    double coords[3];
+
+public:
+    Point(double x, double y, double z) {
+        setX(x);
+        setY(y);
+        setZ(z);
+    };
+    Point() {
+        setX(0);
+        setY(0);
+        setZ(0);
+    }
+    ~Point() {};
+    double x() {return coords[0];};
+    double y() {return coords[1];};
+    double z() {return coords[2];};
+    void setX(double x) {
+        coords[0] = x;
+    };
+    void setY(double y) {
+        coords[1] = y;
+    };
+    void setZ(double z) {
+        coords[2] = z;
+    };
+    double &operator[](unsigned int x) {
+        return coords[x];
+    }
+};
+
+
+class Vector: public Point {
+
+public:
+    Vector(double x, double y, double z);
+    Vector();
+    ~Vector() {};
+    double norm();
+    void normalize();
+    static Vector crossProduct(Vector u, Vector v);
+    static Vector crossProduct(Point a, Point b, Point c);
+    static double dotProduct(Vector u, Vector v);
+};
+
+
+class Quaternion {    
+public:
+    double w,x,y,z;
+
+    Quaternion(double aw, double ax, double ay, double az) {
+        w=aw;
+        x=ax;
+        y=ay;
+        z=az;
+    }
+    Quaternion() {
+        w=x=y=z=0;
+    }
+    ~Quaternion() {};
+    double norm();
+    void normalize();
+    friend Quaternion operator*(Quaternion q, Quaternion r);
+    Quaternion conjugate();
+};
+
+
+#endif
